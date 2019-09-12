b39bbf2d573023d7cec724b5c1ab6d0f80ebd5d3
zickcode <zickcode@foxmail.com>
2019-09-12 14:16:00 +0800

    外包

--- wb_2019.09.10/src/app/assets/js/main.js
+++ wb_2019.09.10/src/app/assets/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 $(function () {
+  //swiper
   var mySwiper = new Swiper('.swiper-container', {
     autoplay: true,
     loop: true,
@@ -9,5 +10,13 @@ $(function () {
       el: '.swiper-pagination',
       clickable: true
     }
+  });
+
+  $('.not-open').on('click', function () {
+    alert('敬请期待');
+  });
+
+  $('.j-news-nav').find('a').on('click', function () {
+    $(this).addClass('on').siblings().removeClass('on');
   });
 });
--- wb_2019.09.10/src/app/js/main.js
+++ wb_2019.09.10/src/app/js/main.js
@@ -1,11 +1,20 @@
 $(function(){
+  //swiper
 	var mySwiper = new Swiper ('.swiper-container', {
-        autoplay: true,
-        loop: true,
-        allowTouchMove: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        },
-      })    
+    autoplay: true,
+    loop: true,
+    allowTouchMove: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  });
+
+  $('.not-open').on('click', function(){
+    alert('敬请期待');
+  })
+
+  $('.j-news-nav').find('a').on('click', function(){
+    $(this).addClass('on').siblings().removeClass('on');
+  })
 });
